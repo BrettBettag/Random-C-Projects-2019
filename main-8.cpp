6fd8cbb1420836c0671f9e8dacd395b2f5891f9a
BrettBettag <62623926+BrettBettag@users.noreply.github.com>
2021-08-04 07:15:28 -0500

    Add files via upload
    Extra project that wouldn't download. 
    
    
    // overloaded functions#include <iostream>using namespace std;int sum (int a, int b){return a+b;}double sum (double a, double b){return a+b;}int main (){cout << sum (10,20) << '\n';cout << sum (1.0,1.5) << '\n';return 0;}

---
+++ main-8.cpp
@@ -0,0 +1,44 @@
+class storable
+{
+        public:
+        storable(const char*);
+        virtual void read()=0; //this becomes pure virtual making storable an abstract
+        virtual void write(); //class
+        virtual ~storable();
+        private:
+        ....S
+}
+
+class transmitter: public virtual storable
+{
+        public:
+        void write()
+        {
+                read();
+                ....
+        }
+}
+
+class receiver: public virtual storable
+{
+        public:
+        void read();
+}
+
+class radio: public transmitter, public receiver
+{
+        public:
+        ...
+}
+
+int main()
+{
+        radio *rad = new radio();
+        receiver *r1 = rad;
+        transmitter *r2 =rad;
+
+        rad->write();
+        r1->write();
+        r2->write();
+        return 1;
+}
